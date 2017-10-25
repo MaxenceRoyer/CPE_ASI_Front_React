fd3642f035cd68b0df9a759cd74626dc185ecd5c
Royer Maxence <A602147@fr01.awl.atosorigin.net>
2017-10-25 15:03:25 +0200

    [MRR] Init of the project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './lib/bootstrap/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (<div className="App">
+      <header className="App-title">
+        <h1>Our App</h1>
+      </header>
+      <section>
+        <section className="col-md-12">
+
+
+
+        </section>
+      </section>
+    </div>);
+  }
+
+}
+export default App;
