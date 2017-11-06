75a846a5806e81ab2d4001eaacd0710edf598284
Royer Maxence <A602147@fr01.awl.atosorigin.net>
2017-11-06 16:03:53 +0100

    [MRR] App structure

---
+++ src/components/browsePresentationPanel/containers/BrowsePresentationPanel.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './browsePresentationPanel.css';
+
+class BrowsePresentationPanel extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (<div></div>);
+  }
+
+}
+export default BrowsePresentationPanel;
---
+++ src/components/common/presentation/containers/Presentation.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './presentation.css';
+
+class Presentation extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (<div></div>);
+  }
+
+}
+export default Presentation;
