66fbbb7bb2f97d169e623f48e171d690bfdb8899
Florian CROISOT <florian.croisot@gmail.fr>
2017-11-08 15:13:51 +0100

    [FCT] Slid displaying with working texts modification

--- src/components/common/presentation/containers/Presentation.js
+++ src/components/common/presentation/containers/Presentation.js
@@ -1,39 +1,34 @@
 import React, { Component } from 'react';
 import './presentation.css';
 import Slid from '../../slid/containers/Slid';
-import EditMetaSlid from '../components/EditMetaPres';
+
 
 class Presentation extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      slidTitle : "Test",
-      slidDescription : "Ceci est un test"
     };
   }
 
-  onChangeTitle(event) {
-    this.setState({slidTitle: event.target.value}).bind(this)
+ /* onChangeTitle(event) {
+    this.setState({slideTitle: event.target.value}).bind(this)
   }
 
   onChangeDescription(event) {
     this.setState({slidDescription: event.target.value}).bind(this)
-  }
+  }*/
 
   render() {
+    
+    /*var onChangeTitle = this.onChangeTitle;
+    var onChangeDescription = this.onChangeDescription;*/
     return (<div>
       <Slid 
         content = {this.props.content}
-        title = {this.state.slidTitle}
-        description = {this.state.slidDescription}
+        displayMode="FULL_MNG"
       />
-      <EditMetaSlid 
-        handleChangeTitle = {this.state.slidTonChangeTitleitle}                     
-        title = {this.state.slidTitle}
-        handleChangeTxt = {this.state.onChangeDescription}            
-        txt = {this.state.slidDescription}
-      />
+      
 
     </div>);
   }
--- src/components/common/slid/containers/Slid.js
+++ src/components/common/slid/containers/Slid.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import './slid.css';
 import ContentInfos from '../../content/containers/contentDisplay/ContentInfos';
 import ContentSimple from '../../content/containers/contentDisplay/ContentSimple';
+import EditMetaSlid from '../components/EditMetaSlid';
 
 class Slid extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      title : this.props.title,
-      descrition : this.props.description
+      title : "Test",
+      description : "Ceci est un test"
     };
   }
-
+/*
   callContentInfos()
   {
     let array_render = [];
@@ -29,18 +30,62 @@ class Slid extends Component {
      
         return array_render;
   }
+*/
+
+/*onChangeTitle(event) {
+    this.setState({title: event.target.value})
+  }
+
+  onChangeDescription(event) {
+    this.setState({description: event.target.value}).bind(this)
+  }*/
+
+displayEditMeta(){
+
+  var onChangeTitle = function(event){
+    this.setState({title: event.target.value})
+  }.bind(this);
+
+  var onChangeDescription = function(event){
+    this.setState({description: event.target.value})
+  }.bind(this);
+
+  let array_render = [];
+
+  switch(this.props.displayMode) {
+    case "FULL_MNG":
+    array_render.push(
+      <EditMetaSlid 
+        handleChangeTitle = {onChangeTitle}                     
+        title = {this.state.title}
+        handleChangeTxt = {onChangeDescription}            
+        txt = {this.state.description}
+      />);
+    break;
+    case "SHORT":
+      array_render.push(<br />);
+    break;
+  }
+  return array_render;
+  
+}
 
   render() {
+
+    let display = this.displayEditMeta();
+
     //var i = this.props.slidId;
     return (
-    <div className="card" width="100%">
-      <h1>{this.state.title}</h1>
-      <span>{this.state.description}</span>
-      <ContentSimple
-            src={this.props.content.src}
-            type={this.props.content.type}
-            />
-    </div>);
+      <div className="card" width="100%">
+        <h1>{this.state.title}</h1>
+        <span>{this.state.description}</span>
+        <ContentSimple
+              src={this.props.content.src}
+              type={this.props.content.type}
+        />
+        {display}
+      </div>
+    );
   }
 
 }
